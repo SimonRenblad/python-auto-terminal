c2170ce34ed83fc5c2a6bfd6ab8c1bc22ce8b645
Simon Renblad <simon.renblad@outlook.com>
2020-12-09 11:02:27 +0800

    changed location of window to left

--- script.py
+++ script.py
@@ -3,16 +3,28 @@ import pyautogui as pag
 pag.PAUSE = 0.5
 pag.FAILSAFE = True # failsafe
 
-#move cursor
-pag.moveTo(120, 1055) # coordinates of lower search box
+#move cursor (legacy)
+#pag.moveTo(120, 1055) # coordinates of lower search box
 
-#click
-pag.click()
+#click (legacy)
+#pag.click()
+
+# open start menu
+pag.hotkey('ctrl', 'esc')
 
 #search
 pag.typewrite("command prompt")
 pag.typewrite(['enter'])
 
+#set window to the left
+pag.keyDown('win')
+pag.keyDown('up')
+pag.keyUp('up')
+pag.keyDown('left')
+pag.keyUp('left')
+pag.keyUp('win')
+
 #type something in the terminal
 pag.typewrite("cd C:\\Users\\simon\\Desktop\\FALL2020\\")
 pag.typewrite(['enter'])
+
